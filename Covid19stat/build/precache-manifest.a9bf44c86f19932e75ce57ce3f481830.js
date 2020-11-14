7ead3fb7797243dc7a7db9ef50bdc87428905083
Joshua Emmanuel <evuetaphajoshua@gmail.com>
2020-11-14 11:54:40 -0800

    added the build folder

---
+++ Covid19stat/build/precache-manifest.a9bf44c86f19932e75ce57ce3f481830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184525b678a8a894771361b3bbb07f3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca40d3278a6b52f46c47",
+    "url": "/static/css/main.2ed1dd46.chunk.css"
+  },
+  {
+    "revision": "4d1022e9f5e01ec663cb",
+    "url": "/static/js/2.5471aae1.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.5471aae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca40d3278a6b52f46c47",
+    "url": "/static/js/main.f129f428.chunk.js"
+  },
+  {
+    "revision": "c9d788ee0a84128f03cf",
+    "url": "/static/js/runtime-main.8b05fdc1.js"
+  },
+  {
+    "revision": "e724fcb88e66a608dbb0d1a51a8b880c",
+    "url": "/static/media/covid19.e724fcb8.jpg"
+  }
+]);
